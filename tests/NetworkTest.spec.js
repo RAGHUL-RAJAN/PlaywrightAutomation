66837d77a3fd1a9a79eec5b3d2336c9a2ebf2cc8
RAGHUL-RAJAN <raghulrajan2015@gmail.com>
2025-04-15 19:11:14 +0530

    Mocking the response

---
+++ tests/NetworkTest.spec.js
@@ -0,0 +1,38 @@
+const { test, expect, request } = require('@playwright/test');
+const {APIUtils} = require('./utils/APIUtils');
+const loginPayload = { userEmail: "jacksparrow007@gmail.com", userPassword: "Qwerty@123" };
+let orderPayload = { orders: [{ country: "Cuba", productOrderedId: "67a8dde5c0d3e6622a297cc8" }] }
+const fakePlayloadOrders = {data:[],message:"No Orders"}
+
+let response;
+test.beforeAll(async () => {
+    const apiContext = await request.newContext();
+    const apiUtils = new APIUtils(apiContext,loginPayload);
+    response = await apiUtils.createOrder(orderPayload);
+})
+
+test("Place to cart", async ({ page }) => {
+    page.addInitScript(value => {
+        window.localStorage.setItem('token', value);
+    }, response.token);
+
+    await page.goto("https://rahulshettyacademy.com/client");
+
+    await page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/67f54f5ffc76541aad25a62f",
+       async route =>{
+            const response = await page.request.fetch(route.request());
+            let body = JSON.stringify(fakePlayloadOrders);
+            route.fulfill(
+                {
+                    response,
+                    body,
+                }
+            )
+        }
+    )
+    
+    await page.locator("button[routerlink*='myorders']").click();
+    await page.pause();
+    await page.locator("tbody").waitFor();
+    const row = await page.locator("tbody tr");
+});
