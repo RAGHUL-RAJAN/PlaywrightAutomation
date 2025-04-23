831283424c2a6dfbd3bf310e48579004c7452787
RAGHUL-RAJAN <raghulrajan2015@gmail.com>
2025-04-23 10:00:14 +0530

    Request interupt

--- tests/NetworkTest2.spec.js
+++ tests/NetworkTest2.spec.js
@@ -2,7 +2,18 @@ const { test, expect } = require("@playwright/test");
 
 test('Security test request intrupt',async({page})=>{
 
-    await page.route("")
+    await page.goto("https://rahulshettyacademy.com/client");
+    await page.getByPlaceholder("email@example.com").fill('jacksparrow007@gmail.com');
+    await page.getByPlaceholder("enter your passsword").fill('Qwerty@123');
+    await page.getByRole("button", { name: "Login" }).click();
+    await page.waitForLoadState("networkidle");
+    await page.locator(".card-body b").first().waitFor();
 
+        await page.locator("button[routerlink*='myorders']").click();
+        await page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=*",
+            route=> route.continue({url:"https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=680865dffc76541aad3b43ee1"}) );
+            await page.locator("button:has-text('View')").first().click();
+            await page.pause();
 
 })
+
