175972bef004038e459f0a36be4afcee5fbda134
RAGHUL-RAJAN <raghulrajan2015@gmail.com>
2025-04-29 11:01:27 +0530

    Excel file validation

--- features/Ecommers.feature
+++ features/Ecommers.feature
@@ -1,5 +1,6 @@
 Feature: Greeting
 
   Scenario: Say hello
+  Given a login to ecommerce application with "username" and "password"
     When the greeter says hello
     Then I should have heard "hello"
---
+++ tests/upload-down.spec.js
@@ -0,0 +1,43 @@
+const ExcelJs = require('exceljs');
+const { test, expect } = require("@playwright/test");
+
+async function writeTest(searchText, replaceText,change, filepath) {
+
+    const workbook = new ExcelJs.Workbook();
+    await workbook.xlsx.readFile(filepath)
+    const worksheet = workbook.getWorksheet('Sheet1');
+    const output = await readExcel(worksheet, searchText);
+
+    const cell = worksheet.getCell(output.row, output.column+change.colChange);
+    cell.value = replaceText;
+    await workbook.xlsx.writeFile(filepath);
+}
+
+
+async function readExcel(worksheet, searchText) {
+    let output = { row: -1, column: -1 };
+    worksheet.eachRow((row, rowNumber) => {
+        row.eachCell((cell, colNumber) => {
+
+            if (cell.value === searchText) {
+                output.row = rowNumber;
+                output.column = colNumber;
+            }
+        })
+    })
+    return output;
+
+}
+
+// writeTest("Apple", 350,{rowChange:0,colChange:2},"C:\\Users\\HP\\Downloads\\ExceldownloadTest.xlsx");
+
+test("upload download excel validation", async ({ page }) => {
+    await page.goto("https://rahulshettyacademy.com/upload-download-test/index.html");
+   const downloadPromise = page.waitForEvent('download');
+    await page.getByRole('button', { name: 'Download' }).click();
+    await downloadPromise;
+    writeTest("Apple", 350,{rowChange:0,colChange:2},"C:\\Users\\HP\\Downloads\\download.xlsx");
+
+    await page.locator("#fileinput").click();
+    await page.locator("#fileinput").setInputFiles("C:\\Users\\HP\\Downloads\\download.xlsx");
+}) 
